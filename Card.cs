2aeee5c0057ddd65d2c2ef86e6d4952c813c48c6
Cliff Crerar <cliff.crerar@gmail.com>
2023-03-03 00:13:05 +0200

    added readme

--- Card.cs
+++ Card.cs
@@ -3,35 +3,41 @@ namespace addmup
 {
 	public class Card
 	{
-		public int Score { get; }
-		public string Value { get; }
+		public int Value { get; }
+		public string CardValue { get; }
 		public bool Picture { get; }
 		public string SuitName { get; }
-		public string SuiteCode { get; }
-        public string? PictureType { get; }
+		public string SuitCode { get; }
+        public int SuitValue { get; }
+        public string? PictureName { get; set; }
 
         public Card(string cardCode) 
 		{
-			SuiteCode = cardCode.Substring(cardCode.Length - 1);
+			SuitCode = cardCode.Substring(cardCode.Length - 1);
+
 			SuitName = assignSuitName();
 
-            Value = cardCode.Substring(0, cardCode.Length - 1);
+            SuitValue = assignSuiteValue();
 
-            Picture = false;
+            CardValue = cardCode.Substring(0, cardCode.Length - 1);
 
-            PictureType = null;
+            Picture = isCardPicture();
 
-            Score = 0;
+            Value = assignCardScore();
 
             Console.WriteLine($"Dealing Card {cardCode}");
-			Console.WriteLine(SuiteCode);
+			Console.WriteLine(SuitCode);
             Console.WriteLine(SuitName);
+            Console.WriteLine(SuitValue);
+            Console.WriteLine(CardValue);
             Console.WriteLine(Value);
+            Console.WriteLine(Picture);
+            Console.WriteLine(PictureName);
         }
 
 		private string assignSuitName()
         {
-            switch (SuiteCode)
+            switch (SuitCode)
             {
                 case "H": return "Hearts";
                 case "D": return "Daimonds";
@@ -41,9 +47,33 @@ namespace addmup
             }
         }
 
-		private string assignCardScore()
-		{
+        private int assignSuiteValue()
+        {
+            switch (SuitCode)
+            {
+                case "H": return 3;
+                case "D": return 2;
+                case "C": return 1;
+                case "S": return 4;
+                default: return 0;
+            }
+        }
 
+        private bool isCardPicture()
+        {
+            return new List<string>() { "A", "J", "K", "Q" }.Exists(e => e == CardValue);
+        }
+
+		private int assignCardScore()
+		{
+            switch(CardValue)
+            {
+                case "A": PictureName = "Ace"; return 11;
+                case "J": PictureName = "Jack"; return 11;
+                case "Q": PictureName = "Queen"; return 12;
+                case "K": PictureName = "King"; return 13;
+                default: return int.Parse(CardValue);
+            }
 		}
 
     }
